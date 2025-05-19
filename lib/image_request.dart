e8a933ecab55623386f88937b9a9c80628dfcd11
Muhammad Awais <muhammad.awais.20024@gmail.com>
2025-05-19 07:57:08 +0500

    Add Windows support for lung_sense application
    - Created CMakeLists.txt for Windows build configuration.
    - Added manifest.json for web application settings.
    - Implemented Windows runner files including main application logic and window management.
    - Integrated Flutter plugin registration for file selector functionality.
    - Added resource files including application icon and version information.
    - Established utility functions for console management and command line argument handling.
    - Configured project to support high DPI settings and dark mode.

---
+++ lib/image_request.dart
@@ -0,0 +1,122 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:lung_sense/analyze.service.dart';
+import 'package:lung_sense/result.dart';
+
+class ImageRequest extends StatefulWidget {
+  const ImageRequest({super.key});
+
+  @override
+  State<ImageRequest> createState() => _ImageRequestState();
+}
+
+class _ImageRequestState extends State<ImageRequest> {
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage(ImageSource source) async {
+    try {
+      final pickedFile = await _picker.pickImage(source: source);
+      if (pickedFile != null) {
+        File image = File(pickedFile.path);
+        StreamedResponse response = await Analyze.analyzeImage(image.path);
+        if (response.statusCode == 200) {
+          Map<String, dynamic> data =
+              jsonDecode(await response.stream.bytesToString())
+                  as Map<String, dynamic>;
+          if (mounted) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder:
+                    (context) =>
+                        Result(result: data['detection'], image: image),
+              ),
+            );
+          }
+        } else {
+          debugPrint(response.reasonPhrase);
+        }
+      } else {}
+    } catch (e) {
+      debugPrint('$e');
+    }
+  }
+
+  Future<void> _takePicture() async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: ImageSource.camera,
+        preferredCameraDevice: CameraDevice.rear,
+      );
+      if (pickedFile != null) {
+        debugPrint(pickedFile.path);
+        StreamedResponse response =
+            Analyze.analyzeImage(pickedFile.path) as StreamedResponse;
+        if (response.statusCode == 200) {
+          debugPrint(await response.stream.bytesToString());
+        } else {
+          debugPrint(response.reasonPhrase);
+        }
+      } else {}
+    } catch (e) {
+      debugPrint('$e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          children: [
+            Image.asset("assets/logo-short.png"),
+            const SizedBox(height: 40),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(16.0)),
+              ),
+              child: TextButton(
+                onPressed: () => _takePicture(),
+                child: const Text(
+                  "Take picture",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(16.0)),
+              ),
+              child: TextButton(
+                onPressed: () => _pickImage(ImageSource.gallery),
+                child: const Text(
+                  "Select an image",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:lung_sense/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
